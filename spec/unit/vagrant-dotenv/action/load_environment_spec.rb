4735d697df2ff2faef554cdc755eefeadcbe8f59
John Bellone <john.bellone.jr@gmail.com>
2014-03-13 09:00:57 -0400

    Stub out some additional specs to write.

---
+++ spec/unit/vagrant-dotenv/action/load_environment_spec.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+require 'spec_helper'
+require 'vagrant-dotenv/action'
+
+describe VagrantPlugins::Dotenv::Action::LoadEnvironment do
+  let(:app) { lambda { |env| } }
+  subject { described_class.new(app, {}) }
+  
+  let(:proxy_config) { double('proxy_config').tap {|p| p.stub(enabled: enabled)} }
+  let(:config) { double('config').tap {|c| c.stub(proxy: proxy_config)} }
+  let(:machine) { double('machine').tap {|m| m.stub(:config).and_return(config)} }
+end
